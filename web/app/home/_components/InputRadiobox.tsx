30f7bc0c7a263a366514db7e113e3cbd1ef48d39
Dennis <denn_c@outlook.com>
2024-04-20 20:30:47 -0700

    modified style of input Radiobox

--- web/app/home/_components/InputRadiobox.tsx
+++ web/app/home/_components/InputRadiobox.tsx
@@ -6,7 +6,7 @@ export default function InputRadiobox({
     onChange,
     disabled = false,
     required = false,
-    checked = false,
+    checked,
 }: {
     id: string;
     labelText: string;
@@ -16,27 +16,32 @@ export default function InputRadiobox({
     checked: boolean
 }) {
     return (
-        <div className="flex items-center mb-4">
-            <label htmlFor={"checkbox-" + id} className="ms-2 text-sm font-medium text-gray-400 border-gray-600 focus:border-blue-500 focus:ring-blue-500">{labelText}</label>
-            <input
-                id={id + "-true"}
-                type="radio"
-                name={id}
-                value="true"
-                checked={checked === true}
-                disabled={disabled}
-                required={required}
-                onChange={onChange}
-                className={clsx(["w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"])}
-            />
+        <div className="mb-4">
+            <div className="flex items-center">
+                <label htmlFor={"checkbox-" + id} className="ms-2 text-sm font-medium text-gray-400 border-gray-600 focus:border-blue-500 focus:ring-blue-500">{labelText}</label>
+            </div>
+            <div className="flex items-center">
+                <input
+                    id={id + "-true"}
+                    type="radio"
+                    name={id}
+                    value="true"
+                    disabled={disabled}
+                    required={required}
+                    onChange={onChange}
+                    className={clsx(["w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"])}
+                />
             <label htmlFor={id + "-true"} className="ms-2 text-sm font-medium text-gray-400 border-gray-600 focus:border-blue-500 focus:ring-blue-500">True</label>
 
+            </div>
+            <div className="flex items-center">
+                
+            </div>
             <input
                 id={id + "-false"}
                 type="radio"
                 name={id}
                 value="false"
-                checked={checked === true}
                 disabled={disabled}
                 required={required}
                 onChange={onChange}
